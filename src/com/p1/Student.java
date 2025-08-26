4a760bb07f0abd2d30edf5b70cd24e1f41c8f2c4
shrikantmosalgi <shrikantmosalgi1997@gmail.com>
2025-08-26 15:21:06 +0530

    taluka

--- src/com/p1/Student.java
+++ src/com/p1/Student.java
@@ -9,12 +9,12 @@ public class Student {
 
 	private String city;
 	private String village;
-<<<<<<< HEAD
+
 	private String taluka;
-=======
+
 	
 	private String district;
->>>>>>> 4a3a3f0c6a06be35de267115dfbb0bfa547a7295
+
 
 
 }
